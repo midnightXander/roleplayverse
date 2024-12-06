2e6db492a4bee7019274a5291ce034d3018fa45f
Xander <denzelwashington913@gmail.com>
2024-12-07 00:33:22 +0100

    re-configured websockets for production

--- para/settings.py
+++ para/settings.py
@@ -111,24 +111,24 @@ ASGI_APPLICATION = 'para.routing.application'
 
 
 REDIS_URL = os.environ.get('REDIS_URL')
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer' if REDIS_URL else 'channels.layers.InMemoryChannelLayer',
-        'CONFIG': {
-            "hosts": [
-                (REDIS_URL),
-                #("127.0.0.1", 6379)
-                ],  # Update with your Redis host and port
-        } if REDIS_URL else {},
-
-    }
-}
 # CHANNEL_LAYERS = {
 #     'default': {
-#         'BACKEND': 'channels.layers.InMemoryChannelLayer',
-        
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer' if REDIS_URL else 'channels.layers.InMemoryChannelLayer',
+#         'CONFIG': {
+#             "hosts": [
+#                 (REDIS_URL),
+#                 #("127.0.0.1", 6379)
+#                 ],  # Update with your Redis host and port
+#         } if REDIS_URL else {},
+
 #     }
 # }
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
