f7293937b932994ffbaf6b954c68c1eebb157484
Xander <denzelwashington913@gmail.com>
2024-12-07 10:42:04 +0100

    re-configured websockets for production

--- core/consumers.py
+++ core/consumers.py
@@ -15,7 +15,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, code):
-        self.channel_layer.group_discard(self.group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data=None, bytes_data=None):
         
