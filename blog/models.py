03ce5f386ee5b61a624d5bb85d36b81e51c3cb6d
Ngaikam Alex <alexngaikama913@gmail.com>
2025-05-03 11:28:28 +0100

    modified blog

--- blog/models.py
+++ blog/models.py
@@ -9,12 +9,18 @@ class BlogPost(models.Model):
     category = models.CharField(choices = [
         (i,i) for i in categories
     ], max_length=30)
-    title = models.CharField(max_length=50)
-    leading = models.CharField(max_length=60)
+    title = models.CharField(max_length=150)
+    leading = models.CharField(max_length=200)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User,models.CASCADE, null=True)
     image = models.ImageField(null=True,upload_to='blog_post_covers/')
+    keyword1 = models.CharField(max_length=100, null=True, blank=True)
+    keyword2 = models.CharField(max_length=100, null=True, blank=True)
+    keyword3 = models.CharField(max_length=100, null=True, blank=True)
+    language = models.CharField(max_length=100, null=True, blank=True)
+    meta_description = models.CharField(max_length=300, null=True, blank=True)
+
 
     def __str__(self):
         """string representation of the blog's post"""
