8ecf86bf359c7c7d93e6d2f86ba82f1a4668f8ff
Ngaikam Alex <alexngaikama913@gmail.com>
2025-08-10 11:14:16 +0100

    updated blog posts 4

--- events/admin.py
+++ events/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 admin.site.register(Tournament)
 admin.site.register(TournamentRequest)
+admin.site.register(TournamentBattle)
 admin.site.register(TournamentRefreeRequest)
 admin.site.register(FighterTournament)
 admin.site.register(RefreeTournament)
